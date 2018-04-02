479f9cf6e76441be4a551a4d00eecb3d2fa22474
Jeno Csiga <csiga.jeno@gmail.com>
2018-04-02 11:25:49 +0200

    Kivonas 2

--- munka.php
+++ munka.php
@@ -18,14 +18,11 @@ print $a + $b + $c;
 // Kivonás
 //------------------
 
-$x = 35;
-$y = 19;
-$z = 37;
+$x = 15;
+$y = 59;
+$z = 370;
 
-print "<br /><br />" . $x - $y;
-
-print "Konfliktus lesz...";
-
+print "<br /><br />" . $z - $y - $x;
 
 
 
